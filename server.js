50ccfd5dbc0ae1b74e8e5eaba035ec4e0cacf9c1
VoidChips <voidchips@gmail.com>
2019-06-09 00:25:44 -0500

    get user info and post new user info

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const users = [
+    {
+        email: 'void@gmail.com',
+        username: 'void',
+        password: '123'
+    },
+    {
+        email: 'test@gmail.com',
+        username: 'test',
+        password: '1234'
+    }
+]
+
+app.get('/', (req, res) => { res.send(users); });
+
+app.post('/register', (req, res) => {
+    let isFound = false;
+    for (user of users) {
+        if (user.email === req.body.email) {
+            isFound = true;
+        }
+    }
+    if (isFound === true) {
+        res.status(400).send('user already exists');
+    }
+    else {
+        users.push(req.body);
+        res.send(users);
+    }
+    
+});
+
+app.listen(3000, () => {
+    console.log(`app is running on port 3000`);
+});
