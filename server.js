f7e909818aabc8cafaff0273bba0cd7f3017191e
VoidChips <voidchips@gmail.com>
2019-06-12 04:26:19 -0500

    improved login and register, only send usernames

--- server.js
+++ server.js
@@ -19,23 +19,50 @@ const users = [
     }
 ]
 
-app.get('/users', (req, res) => { 
-    res.send(users);
+app.get('/users', (req, res) => {
+    let usernames = [];
+    for (user of users) {
+        usernames.push(user.username);
+    }
+    res.send(usernames);
+});
+
+app.post('/login', (req, res) => {
+    let isFound = false;
+    for (user of users) {
+        if (user.username === req.body.username && user.password === req.body.password) {
+            isFound = true;
+            break; // stop loop after user found
+        }
+    }
+    if (isFound) {
+        res.send({ 'result': 'found' });
+    }
+    else {
+        res.status(400).send({ 'result': 'not found' });
+    }
 });
 
 app.post('/register', (req, res) => {
     let isFound = false;
-    for (user of users) {
-        if (user.email === req.body.email || user.username === req.body.username) {
-            isFound = true;
+    const arr = Object.keys(req.body);
+    if (arr[0] === 'email' && arr[1] === 'username' && arr[2] === 'password') {
+        for (user of users) {
+            if (user.email === req.body.email || user.username === req.body.username) {
+                isFound = true;
+                break; // don't need to continue the loop after user found
+            }
+        }
+        if (!isFound) {
+            res.send({ 'result': 'success' });
+            users.push(req.body);
+        }
+        else {
+            res.status(400).send({ 'result': 'user already exists' });
         }
     }
-    if (!isFound) {
-        users.push(req.body);
-        res.send('register successful');
-    }
     else {
-        res.status(400).send('user already exists');
+        res.status(400).send({ 'result': 'invalid' });
     }
 });
 
