f8486e90ee2bd34b75189a3f104743182beef498
VoidChips <voidchips@gmail.com>
2019-06-16 01:28:22 -0500

    added products

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const users = [
         email: 'void@gmail.com',
         username: 'void',
         password: '123',
-        balance: 200
+        balance: 1000
     },
     {
         email: 'test@gmail.com',
@@ -24,7 +24,30 @@ const users = [
         password: '1234',
         balance: 100
     }
-]
+];
+
+const products = [
+    {
+        name: 'Instant Ramen Noodles',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/915AEp17FaL._SL1500_.jpg',
+        price: 0.99
+    },
+    {
+        name: 'Dell Desktop PC Combo',
+        image: 'https://cdn11.bigcommerce.com/s-w5trgcbv/images/stencil/608x608/products/5945/49088/35049sku__97320.1545069629.jpg?c=2',
+        price: 600
+    },
+    {
+        name: 'Xbox One Controller',
+        image: 'https://m.media-amazon.com/images/S/aplus-media/mg/e78341e6-37be-49a8-9044-f9389a0384de.jpg',
+        price: 50
+    },
+    {
+        name: 'Apples (1 lb)',
+        image: 'https://images-prod.healthline.com/hlcmsresource/images/topic_centers/Do_Apples_Affect_Diabetes_and_Blood_Sugar_Levels-732x549-thumbnail.jpg',
+        price: 1.5
+    }
+];
 
 app.get('/users', (req, res) => {
     let usernames = [];
@@ -32,6 +55,10 @@ app.get('/users', (req, res) => {
         usernames.push(user.username);
     }
     res.send(usernames);
+});
+
+app.get('/products', (req, res) => {
+    res.send(products);
 });
 
 // send user's balance
