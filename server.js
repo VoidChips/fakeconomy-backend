eef8293bdbd516101a92aec251088cc31f9cdcca
VoidChips <voidchips@gmail.com>
2019-07-05 22:26:35 -0500

    fixed email verification

--- server.js
+++ server.js
@@ -32,8 +32,9 @@ const products = [
     }
 ];
 
+// get verified users
 app.get('/users', (req, res) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
+    pool.query('SELECT * FROM users WHERE verified = $1 ORDER BY id ASC', [true], (err, results) => {
         if (err) { throw err; }
         let usernames = [];
         const users = results.rows;
@@ -79,11 +80,18 @@ app.post('/login', (req, res) => {
             res.status(404).send({ 'error': 'unknown' });
         }
         const user = results.rows;
+        // check if there are any user with the username
         if (user.length) {
-            if (username === user[0].username && password === user[0].password) {
+            // check if the login info is correct and the user if verified
+            if (username === user[0].username && password === user[0].password && user[0].verified === true) {
                 const id = user[0].id.toString();
                 res.send({ 'id': id });
             }
+            // check if the login info is correct and the user is unverified
+            else if (username === user[0].username && password === user[0].password && user[0].verified === false) {
+                res.status(404).send({ 'error': 'unverified' })
+            }
+            // login info is incorrect
             else {
                 res.status(404).send({ 'error': 'incorrect info' });
             }
@@ -94,6 +102,35 @@ app.post('/login', (req, res) => {
 
     });
 });
+
+const generateVerificationCode = () => {
+    return Math.floor(Math.random() * 999999) + 100000;
+}
+
+const sendVerificationMail = (email, username, code) => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: email_config.address,
+            pass: email_config.password
+        }
+    });
+
+    const mailOptions = {
+        from: `${email_config.name} <${email_config.address}>`,
+        to: email,
+        subject: 'Verify your account',
+        text: `${username}, Your verification code is ${code}`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
 app.post('/register', (req, res) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
@@ -107,35 +144,9 @@ app.post('/register', (req, res) => {
             }
         }
         if (!isFound) {
-            let code = Math.floor(Math.random() * 999999) + 100000;
-            pool.query('INSERT INTO users (email, username, password, verification_code) VALUES ($1, $2, $3, $4)', [email, username, password, code], (err, results) => {
+            pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [email, username, password], (err, results) => {
                 if (err) { throw err; }
-
-                const transporter = nodemailer.createTransport({
-                    service: 'Gmail',
-                    auth: {
-                        user: email_config.address,
-                        pass: email_config.password
-                    }
-                });
-
-                const mailOptions = {
-                    from: `${email_config.name} <${email_config.address}>`,
-                    to: email,
-                    subject: 'Verify your account',
-                    text: `${username}, Your verification code is ${code}`
-                };
-
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                });
-                pool.query('SELECT id FROM users WHERE username = $1', [username], (err, results) => {
-                    res.send({ 'result': results.rows[0].id });
-                });
+                res.send({ result: 'user registered' });
             });
         }
         else {
@@ -145,20 +156,33 @@ app.post('/register', (req, res) => {
 });
 
 app.put('/verify', (req, res) => {
-    const { id } = req.body;
+    const { username } = req.body;
     let { code } = req.body;
-    code = Number(code);
-    pool.query('SELECT verification_code FROM users WHERE id = $1', [id], (err, results) => {
+    pool.query('SELECT email, verification_code FROM users WHERE username = $1', [username], (err, results) => {
         if (err) { throw err; }
-        // verify user if code is valid
-        if (code === results.rows[0].verification_code) {
-            pool.query('UPDATE users SET verified = TRUE WHERE id = $1', [id], (err, results) => {
-                if (err) { res.status(400).send(err); }
-                res.send({ verified: 'true' });
-            })
+        if (code === 'reset') {
+            const email = results.rows[0].email;
+            const new_code = generateVerificationCode();
+            pool.query('UPDATE users SET verification_code = $1 WHERE username = $2', [new_code, username], (err, results) => {
+                if (err) { throw err; }
+                sendVerificationMail(email, username, new_code);
+                res.send({ result: 'new code' });
+
+            });
         }
         else {
-            res.status(404).send({ verified: 'false' });
+            // code variable is a string, so it needs to be converted to a number;
+            code = Number(code);
+            // verify user if code is valid
+            if (code === results.rows[0].verification_code) {
+                pool.query('UPDATE users SET verified = TRUE WHERE username = $1', [username], (err, results) => {
+                    if (err) { res.status(400).send(err); }
+                    res.send({ verified: 'true' });
+                })
+            }
+            else {
+                res.status(404).send({ verified: 'false' });
+            }
         }
     });
 });
