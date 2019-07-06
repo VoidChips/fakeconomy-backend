9077abff71d362a962ec6ccc131ab137d824058a
VoidChips <voidchips@gmail.com>
2019-07-06 01:36:58 -0500

    made image request and improved products request

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pool = require('../database-config/fakeconomy-config');
 const nodemailer = require('nodemailer');
+// const path = require('path');
+
+const pool = require('../database-config/fakeconomy-config');
 const email_config = require('../email_config');
+const root = require('./root');
+// root is the absolute path of the project directory
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const products = [
-    {
-        name: 'Instant Ramen Noodles',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/915AEp17FaL._SL1500_.jpg',
-        price: 0.99
-    },
-    {
-        name: 'Dell Desktop PC Combo',
-        image: 'https://cdn11.bigcommerce.com/s-w5trgcbv/images/stencil/608x608/products/5945/49088/35049sku__97320.1545069629.jpg?c=2',
-        price: 600
-    },
-    {
-        name: 'Xbox One Controller',
-        image: 'https://m.media-amazon.com/images/S/aplus-media/mg/e78341e6-37be-49a8-9044-f9389a0384de.jpg',
-        price: 50
-    },
-    {
-        name: 'Apples (1 lb)',
-        image: 'https://images-prod.healthline.com/hlcmsresource/images/topic_centers/Do_Apples_Affect_Diabetes_and_Blood_Sugar_Levels-732x549-thumbnail.jpg',
-        price: 1.5
-    }
-];
 
 // get verified users
 app.get('/users', (req, res) => {
@@ -56,8 +37,17 @@ app.get('/account/:id', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-    res.send(products);
+    pool.query('SELECT * FROM products', (err, results) => {
+        if (err) { throw err; }
+        const products = results.rows;
+        res.send(products);
+    })
 });
+
+app.get('/image/:name', (req, res) => {
+    const name = req.params.name;
+    res.sendFile(`images/${name}`, { root: root });
+})
 
 // update user's balance
 app.post('/update_balance', (req, res) => {
